dbec3244e24a210f4801c9ce8dc455bdb8e2402d
Sebastián <sebastiangr456@gmail.com>
2022-04-20 18:03:20 +0200

    Home created.

---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Sebastián González Ríos">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Bienvenido {{ Auth::user()->username }}</h1>
+    <form action="{{ route('logout') }}" method="POST">
+        @csrf
+        <input type="submit" value="logout">
+    </form>
+</body>
+</html>
