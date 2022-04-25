9cf03e74687edd36c5faeb8313da0fd157882a74
srcbas <sebastiangr456@gmail.com>
2022-04-26 00:05:05 +0200

    Fix incompatibilization Turbolinks with Livewire 2.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -39,5 +39,6 @@
         </div>
 
         @livewireScripts
+        <script src="https://cdn.jsdelivr.net/gh/livewire/turbolinks@v0.1.x/dist/livewire-turbolinks.js" data-turbolinks-eval="false" data-turbo-eval="false"></script>
     </body>
 </html>
