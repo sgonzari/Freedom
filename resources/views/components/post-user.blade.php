b6386dce4e18b509720d80d31ecc0abe555685c9
srcbas <sebastiangr456@gmail.com>
2022-04-27 19:50:26 +0200

    Admin can delete posts on user's profile.

--- resources/views/components/post-user.blade.php
+++ resources/views/components/post-user.blade.php
@@ -14,7 +14,7 @@
                     <td>{{ $post->user->first()->username }}</td>
                     <td>{{ $post->content }}</td>
                     <td>{{ $post->created_at->format('d/m/Y') }}</td>
-                    @if ($post->user->first()->id_user == Auth::user()->id_user)
+                    @if (($post->user->first()->id_user == Auth::user()->id_user) OR (Auth::user()->rol->first()->id_rol > 1))
                     <td><button wire:click="$emit('delete', {{ $post->id_post }})">Eliminar</button></td>
                     @endif
                 </tr>
