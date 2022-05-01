f0e3678256e907bace0a1b7b2b9662b7216f3e83
Sebastián <sebastiangr456@gmail.com>
2022-05-01 12:59:44 +0200

    Livewire views with a good sentence.

--- resources/views/components/post-create.blade.php
+++ resources/views/components/post-create.blade.php
@@ -1,6 +1,8 @@
-<form wire:submit.prevent="store">
-    <x-label value="Contenido:"/>
-    <x-input type="text" placeholder="What's happening?" wire:model.defer="content"/>
-    <h1>{{ $content }}</h1>
-    <button type="submit">Enviar</button>
-</form>
+<div>
+    <form wire:submit.prevent="store">
+        <x-label value="Contenido:"/>
+        <x-input type="text" placeholder="What's happening?" wire:model.defer="content"/>
+        <h1>{{ $content }}</h1>
+        <button type="submit">Enviar</button>
+    </form>
+</div>
--- resources/views/components/post-home.blade.php
+++ resources/views/components/post-home.blade.php
@@ -1,26 +1,28 @@
-@if (!empty($posts))
-    <table>
-        <thead>
-            <tr>
-                <th>Usuario</th>
-                <th>Content</th>
-                <th>Creación</th>
-                <th>Eliminar</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($posts as $post)
+<div>
+    @if (!empty($posts))
+        <table>
+            <thead>
                 <tr>
-                    <td>{{ $post->user->first()->username }}</td>
-                    <td>{{ $post->content }}</td>
-                    <td>{{ $post->created_at->format('d/m/Y') }}</td>
-                    @if (($post->user->first()->id_user == Auth::user()->id_user) OR (Auth::user()->rol->first()->id_rol > 1))
-                    <td><button wire:click="$emit('deletePost', {{ $post->id_post }})">Eliminar</button></td>
-                    @endif
+                    <th>Usuario</th>
+                    <th>Content</th>
+                    <th>Creación</th>
+                    <th>Eliminar</th>
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
-@else
-    <h1>No hay publicaciones</h1>
-@endif
+            </thead>
+            <tbody>
+                @foreach ($posts as $post)
+                    <tr>
+                        <td>{{ $post->user->first()->username }}</td>
+                        <td>{{ $post->content }}</td>
+                        <td>{{ $post->created_at->format('d/m/Y') }}</td>
+                        @if (($post->user->first()->id_user == Auth::user()->id_user) OR (Auth::user()->rol->first()->id_rol > 1))
+                        <td><button wire:click="$emit('deletePost', {{ $post->id_post }})">Eliminar</button></td>
+                        @endif
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    @else
+        <h1>No hay publicaciones</h1>
+    @endif
+</div>
