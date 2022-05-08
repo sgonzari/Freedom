6012c53a8bc40e1db4f4c223dd7b86a0bbd5075c
Sebastián <sebastiangr456@gmail.com>
2022-05-08 17:44:58 +0200

    Notification bug fixed.

--- app/Http/Livewire/PostView.php
+++ app/Http/Livewire/PostView.php
@@ -40,7 +40,7 @@ class PostView extends Component
                     for ($i = 1 ; $i <= count($phrases) ; $i++) {
                         if (($i == count($phrases)) OR ($i % 2 == 0)) {
                             $username = explode(" ", $phrases[$i-1]) ;
-                            if (!is_null(User::where('username', $username[0])->first())) {
+                            if (!is_null(User::where('username', $username[0])->first()) and (Auth::user()->id_user != User::where('username', $username[0])->first()->id_user)) {
                                 Notification::create([
                                     'fk_user' => Auth::user()->id_user,
                                     'fk_notifier' => User::where('username', $username[0])->first()->id_user,
@@ -52,12 +52,14 @@ class PostView extends Component
                     }
                 }
     
-                Notification::create([
-                    'fk_user' => Auth::user()->id_user,
-                    'fk_notifier' => $this->user->id_user,
-                    'fk_post' => Post::withTrashed()->count(),
-                    'fk_typeNot' => 2
-                ]);
+                if (Auth::user()->id_user != $this->user->id_user) {
+                    Notification::create([
+                        'fk_user' => Auth::user()->id_user,
+                        'fk_notifier' => $this->user->id_user,
+                        'fk_post' => Post::withTrashed()->count(),
+                        'fk_typeNot' => 2
+                    ]);
+                }
     
                 $this->reset('contentComment') ;
             }
