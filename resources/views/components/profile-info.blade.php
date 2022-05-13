ea5bae1bb59b92cc54486f660dee37287b7c7347
Sebastián <sebastiangr456@gmail.com>
2022-05-13 19:39:32 +0200

    Profile view design created.

--- resources/views/components/profile-info.blade.php
+++ resources/views/components/profile-info.blade.php
@@ -1,9 +1,61 @@
-<div>
-    <h1>{{ $user->username }}</h1>
-    <p>{{ $user->description }}</p>
-        @if (Auth::user()->followings()->find($user->id_user))
-            <button wire:click="unfollowUser">Dejar de seguir</button>
-        @else
-            <button wire:click="followUser">Seguir</button>
-        @endif
+<div class="profile">
+    <div class="profile__header">
+        <div class="profile__header--image">
+            <img class="profile__image" src="http://localhost/freedom/public/storage/{{ $user->profile_image }}" />
+        </div>
+        <div class="profile__header--text">
+            <div class="profile__text--name">
+                <span>{{ $user->name }}</span>
+            </div>
+            <div class="profile__text--username">
+                <span>{{ __('@') }}{{ $user->username }}</span>
+            </div>
+            <p class="profile__text--username"></p>
+        </div>
+        <div class="profile__header--buttons">
+            <div class="profile__button">
+                @if (Auth::user()->id_user === $user->id_user)
+                    <button class="profile__button--element" wire:click="editProfile">{{ __('Edit Profile') }}</button>
+                @else
+                    @if (Auth::user()->followings()->find($user->id_user))
+                        <button class="profile__button--element" wire:click="unfollowUser">Dejar de seguir</button>
+                    @else
+                        <button class="profile__button--element" wire:click="followUser">Seguir</button>
+                    @endif
+                @endif
+            </div>
+        </div>
+    </div>
+    <div class="profile__main">
+        <div class="profile__main--description">
+            <p class="profile__description">{{ $user->description }}</p>
+        </div>
+        <div class="profile__main--info">
+            <div class="profile__info--element">
+                <span class="profile__element--icon material-symbols-rounded"> celebration </span>
+                <p class="profile__element--text">{{ date('d-M-Y', strtotime($user->birthday)) }}</p>
+            </div>
+            <div class="profile__info--element">
+                <span class="profile__element--icon material-symbols-rounded"> calendar_month </span>
+                <p class="profile__element--text">{{ $user->created_at->format('d-m-Y') }}</p>
+            </div>
+        </div>
+        <div class="profile__main--count">
+            <p class="profile__count--element">
+                <span class="profile__element--count">{{ $user->followings()->count() }}</span> following</p>
+            <p class="profile__count--element">
+                <span class="profile__element--count">{{ $user->followers()->count() }}</span> followers</p>
+        </div>
+    </div>
+    <div class="profile__footer">
+        <a class="profile__footer--element active" href="{{ route('home') }}">
+            Posts
+        </a>
+        <a class="profile__footer--element" href="{{ route('home') }}">
+            Reposts
+        </a>
+        <a class="profile__footer--element" href="{{ route('home') }}">
+            Likes
+        </a>
+    </div>
 </div>
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -2,9 +2,15 @@
     <x-slot name="title">{{ __('Profile') }}</x-slot>
 
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Profile') }}
-        </h2>
+        <a class="main__header--back" href="{{ route('home') }}">
+            <svg class="main__header--icon" viewBox="0 0 24 24" aria-hidden="true" class="r-18jsvk2 r-4qtqp9 r-yyyyoo r-z80fyv r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-19wmn03">
+                <g>
+                    <path d="M20 11H7.414l4.293-4.293c.39-.39.39-1.023 0-1.414s-1.023-.39-1.414 0l-6 6c-.39.39-.39 1.023 0 1.414l6 6c.195.195.45.293.707.293s.512-.098.707-.293c.39-.39.39-1.023 0-1.414L7.414 13H20c.553 0 1-.447 1-1s-.447-1-1-1z">
+                    </path>
+                </g>
+            </svg>
+        </a>
+        <h2 class="main__header--title">{{ __('Profile') }}</h2>
     </x-slot>
 
     <livewire:profile-component :user="$user"/>
