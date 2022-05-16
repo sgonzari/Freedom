b0c7e103ff258db86e94fd78540ee5f1cb585e46
Sebastián <sebastiangr456@gmail.com>
2022-05-16 14:55:06 +0200

    Reposts and Likes followings user, showed it in Home view.

--- app/Http/Livewire/HomeComponent.php
+++ app/Http/Livewire/HomeComponent.php
@@ -10,20 +10,41 @@ use Livewire\Component;
 
 class HomeComponent extends Component
 {
+    protected $listeners = ['render' => 'render'] ;
+
     public function render() {
         $posts = [] ;
 
+        // Post de usuarios a los que sigue
         $followers = User::find(Auth::user()->id_user)->followings()->get() ;
         foreach ($followers as $follower) {
             foreach ($follower->posts()->get() as $followerPost) {
                 array_push($posts, $followerPost) ;
             }
         }
+        // Repost de usuarios a los que sigue
+        foreach ($followers as $follower) {
+            foreach ($follower->reposts()->get() as $followerPost) {
+                array_push($posts, $followerPost) ;
+            }
+        }
+        // Likes de usuarios a los que sigue
+        foreach ($followers as $follower) {
+            foreach ($follower->likes()->get() as $followerPost) {
+                array_push($posts, $followerPost) ;
+            }
+        }
+
+        // Posts propios
         $userPosts = User::find(Auth::user()->id_user)->posts()->orderBy('created_at')->get() ;
         foreach ($userPosts as $userPost) {
             array_push($posts, $userPost) ;
         }
         
+        usort($posts, function($x, $y) {
+            return $x['created_at'] < $y['created_at'];
+        });
+
         return view('components.home-component', compact('posts'));
     }
 }
