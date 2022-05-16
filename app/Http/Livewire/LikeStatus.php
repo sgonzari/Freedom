a93f274092c0327b9b847e2c32712a2b7312d5ef
Sebastián <sebastiangr456@gmail.com>
2022-05-16 15:55:11 +0200

    Notification on Likes & Reposts created.

--- app/Http/Livewire/LikeStatus.php
+++ app/Http/Livewire/LikeStatus.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Notification;
 use App\Models\Post;
 use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
@@ -18,6 +19,15 @@ class LikeStatus extends Component
     public function addLike () {
         if (!Auth::user()->likes()->find($this->post->id_post)) {
             Auth::user()->likes()->attach($this->post) ;
+
+            if (Auth::user()->id_user != $this->post->user()->first()->id_user) {
+                Notification::create([
+                    'fk_user' => Auth::user()->id_user,
+                    'fk_notifier' => $this->post->user()->first()->id_user,
+                    'fk_post' => $this->post->id_post,
+                    'fk_typeNot' => 4
+                ]);
+            }
         }
     }
     public function deleteLike () {
