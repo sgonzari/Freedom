3a1dad933dcbb2d923b579ad5564e7ff3732b20a
Sebastián <sebastiangr456@gmail.com>
2022-05-31 01:37:14 +0200

    Example of modal responsive nav created.

--- resources/js/main.js
+++ resources/js/main.js
@@ -33,4 +33,22 @@ window.onload = () => {
     Livewire.on('graphLoader', props => {
         GraphsStatisticsLoader(props);
     });
+
+    window.addEventListener('resize', () => {
+        if(window.matchMedia("(max-width: 1021px)").matches) {
+            if(window.matchMedia("(min-width: 720px)").matches) {
+                document.getElementById('profileOptionModal').style.left = (117.328 - ((1021 - window.innerWidth) * 0.328)) + 'px';
+            } else {
+                document.getElementById('profileOptionModal').style.left = 12 + 'px';
+            }
+        } else if(window.matchMedia("(max-width: 1111px)").matches) {
+            document.getElementById('profileOptionModal').style.left = (55 - ((1111 - window.innerWidth) * 0.5)) + 'px';
+        } else if(window.matchMedia("(max-width: 1298px)").matches) {
+            document.getElementById('profileOptionModal').style.left = (113.5 - ((1298 - window.innerWidth) * 0.5)) + 'px';
+        } else if(window.matchMedia("(max-width: 1306px)").matches){
+            document.getElementById('profileOptionModal').style.left = (24 - ((1306 - window.innerWidth) * 0.5)) + 'px';
+        } else {
+            document.getElementById('profileOptionModal').style.left = (306.5 - ((1920 - window.innerWidth) * 0.5)) + 'px';
+        }
+    });
 }
