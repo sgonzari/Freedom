044bf3bdde99ce0a6817b5d5a5bbe58469364636
srcbas <sebastiangr456@gmail.com>
2022-05-31 18:22:32 +0200

    Useless resources removed.

--- routes/web.php
+++ routes/web.php
@@ -16,15 +16,30 @@ use App\Http\Controllers\Auth\AuthenticatedSessionController;
 |
 */
 
+/* INDEX ROUTE */
 Route::get('/', [AuthenticatedSessionController::class, 'create'])->middleware('guest')->name("index") ;
 
-Route::view('/home', "home")->middleware(['banned', 'auth'])->name("home") ;
-Route::view('/notification', "notification")->middleware(['banned', 'auth'])->name("notification") ;
-Route::view('/bookmark', "bookmark")->middleware(['banned', 'auth'])->name("bookmark") ;
-Route::get('/{username}/post/{id_post}', [UserController::class, "post"])->middleware(['banned', 'auth'])->name("post") ;
-Route::view('/admin', "admin")->middleware(['banned', 'auth', 'admin'])->name("admin") ;
+/* BAN ROUTE */
 Route::view('/banned', "banned")->middleware(['auth'])->name("banned") ;
+
+/* HOME ROUTE */
+Route::view('/home', "home")->middleware(['banned', 'auth'])->name("home") ;
+
+/* NOTIFICATION ROUTE */
+Route::view('/notification', "notification")->middleware(['banned', 'auth'])->name("notification") ;
+
+/* BOOKMARK ROUTE */
+Route::view('/bookmark', "bookmark")->middleware(['banned', 'auth'])->name("bookmark") ;
+
+/* POST ROUTE */
+Route::get('/{username}/post/{id_post}', [UserController::class, "post"])->middleware(['banned', 'auth'])->name("post") ;
+
+/* ADMIN ROUTE */
+Route::view('/admin', "admin")->middleware(['banned', 'auth', 'admin'])->name("admin") ;
+
 require __DIR__.'/auth.php';
+
+/* PROFILES ROUTES */
 Route::get('/{username}', [UserController::class, "profile"])->middleware(['banned', 'auth'])->name("profile") ;
 Route::get('/{username}/reposts', [UserController::class, "profileReposts"])->middleware(['banned', 'auth'])->name("profile-reposts") ;
 Route::get('/{username}/likes', [UserController::class, "profileLikes"])->middleware(['banned', 'auth'])->name("profile-likes") ;
